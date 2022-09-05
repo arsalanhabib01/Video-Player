8f59cf3d5b7d6a6162effe9cd1980e00a9a8937c
Arsalan Habib <arsalan.habib@tietoevry.com>
2022-09-05 11:06:34 +0200

    progress bar functioning

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const fullscreenBtn = document.querySelector('.fullscreen');
  
 // Play & Pause ----------------------------------- //
 
-function ShowPlayIcon() {
+function showPlayIcon() {
     playBtn.classList.replace('fa-pause', 'fa-play');
     playBtn.setAttribute('title', 'Play');
 }
@@ -24,17 +24,37 @@ function togglePlay() {
         playBtn.setAttribute('title', 'Pause');
     } else {
         video.pause();
-        ShowPlayIcon();
+        showPlayIcon();
     }
 }
 
 // On Video end, show play button icon
-video.addEventListener('ended', ShowPlayIcon);
+video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;  
+}
+
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX /  progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
 
@@ -48,5 +68,8 @@ video.addEventListener('ended', ShowPlayIcon);
 
 
 // Event listeners
-playBtn.addEventListener('click',togglePlay);
-video.addEventListener('click',togglePlay);
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
